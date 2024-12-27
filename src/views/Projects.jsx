e914bbe706dd584a0d48e3ab0669ecee7950f70c
Shivam Vishwakarma <100554753+CodeOfShivam@users.noreply.github.com>
2024-12-27 19:08:00 +0530

    Update Projects.jsx

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -55,7 +55,7 @@ const Projects = () => {
             >
               <a>
                 <img
-                  className="rounded-t-lg w-full h-[400px] object-cover"
+                  className="rounded-t-lg w-full h-[300px] object-cover"
                   src={el.img}
                   alt={el.name}
                 />
@@ -108,7 +108,7 @@ const Projects = () => {
                   <img
                     src={selectedProject.image[currentImageIndex]}
                     alt={`${selectedProject.name} Image`}
-                    className="w-full h-[300px] object-cover rounded-lg"
+                    className="w-full h-[400px] object-cover rounded-lg"
                   />
                   <button
                     onClick={prevImage}
